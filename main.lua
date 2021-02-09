52520226df79b49763e47f13879899b30447cf05
Olivia <guesso.olivia@gmail.com>
2021-02-08 19:34:12 -0600

    dancing

--- main.lua
+++ main.lua
@@ -20,6 +20,8 @@ function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, windowWidth, windowHeight)
 
+    --this is also from the tutorial
+    animation = newAnimation(love.graphics.newImage("Green-Cap-Character-16x18.png"), 16, 18, 1)
     
 end
 
@@ -36,6 +38,12 @@ function love.update(dt)
         player.x = player.x + PLAYER_SPEED * dt
     end
 
+    --also from tutorial
+    animation.currentTime = animation.currentTime + dt
+    if animation.currentTime >= animation.duration then
+        animation.currentTime = animation.currentTime - animation.duration
+    end
+
 end
 
 
@@ -46,5 +54,24 @@ function love.draw()
     love.graphics.setColor(179/255, 235/255 , 193/255, 210/255) --player?? we can make them a square for now ig
     love.graphics.rectangle('fill', player.x, player.y, 10, 10)
 
+    --also from tutorial
+    local spriteNum = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
+    love.graphics.draw(animation.spriteSheet, animation.quads[spriteNum], 0, 0, 0, 4)
+
     push:finish()
+end
+
+--This is from the love tutorial
+function newAnimation(image, width, height, duration)
+    local animation = {}
+    animation.spriteSheet = image;
+    animation.quads = {};
+    for h = 0, image:getHeight() - height, height do
+        for w = 0, image:getWidth() - width, width do
+            table.insert(animation.quads, love.graphics.newQuad(w,h, width, height, image:getDimensions()))
+        end
+    end
+    animation.duration = duration or 1
+    animation.currentTime = 0
+    return animation
 end
